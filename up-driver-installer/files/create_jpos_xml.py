dba79a5be346ba8b70ce86abbc8e5afb778bfa0a
Anders Näslund <Anders.Naslund@extenda.com>
2018-08-31 17:03:13 +0200

    Add support for more than one jpos entry in file for jpos xml

--- up-driver-installer/files/create_jpos_xml.py
+++ up-driver-installer/files/create_jpos_xml.py
@@ -6,8 +6,8 @@ jpos_file = etree.Element("JposEntries")
 for jpos_entry_file in glob.glob('/opt/JavaPOS/config_xml/*.xml'):
     original = etree.parse(jpos_entry_file)
     root = original.getroot()
-    child = root.find('JposEntry')
-    duplicate = copy.deepcopy(child)
-    jpos_file.append(duplicate)
+    for child in root.findall('JposEntry'):
+        duplicate = copy.deepcopy(child)
+        jpos_file.append(duplicate)
     et = etree.ElementTree(jpos_file)
     et.write('/opt/JavaPOS/config_xml/jpos.xml', pretty_print=True, method='xml', encoding='UTF-8')
