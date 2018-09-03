f1559eeefd4ede3a35d81c8602f983e4b3bce61a
Anders Näslund <Anders.Naslund@extenda.com>
2018-09-03 13:03:07 +0200

    Add doctype to jpos xml and fix bug in support for several jpos entry

--- up-driver-installer/files/create_jpos_xml.py
+++ up-driver-installer/files/create_jpos_xml.py
@@ -1,13 +1,28 @@
 import copy
 import glob
 from lxml import etree
+import StringIO
 
-jpos_file = etree.Element("JposEntries")
+jpos_file = '/opt/JavaPOS/config_xml/jpos.xml'
+
+template = """
+<!DOCTYPE JposEntries PUBLIC "-//JavaPOS//DTD//EN"
+                             "jpos/res/jcl.dtd">
+<JposEntries>
+</JposEntries>
+"""
+
+jpos_template = etree.parse(StringIO.StringIO(template))
+jpos_template.write(jpos_file, pretty_print=True)
+
+jpos_file_parsed = etree.parse(jpos_file)
+jpos_file_root = jpos_file_parsed.getroot()
+
 for jpos_entry_file in glob.glob('/opt/JavaPOS/config_xml/*.xml'):
     original = etree.parse(jpos_entry_file)
     root = original.getroot()
     for child in root.findall('JposEntry'):
         duplicate = copy.deepcopy(child)
-        jpos_file.append(duplicate)
-    et = etree.ElementTree(jpos_file)
-    et.write('/opt/JavaPOS/config_xml/jpos.xml', pretty_print=True, method='xml', encoding='UTF-8')
+        jpos_file_root.append(duplicate)
+jpos_entries = etree.ElementTree(jpos_file_root)
+jpos_entries.write(jpos_file, pretty_print=True, method='xml', xml_declaration=True, encoding='UTF-8')
